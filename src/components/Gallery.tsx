1ee82d56249001cd9f2fb9fb23dd87300e8a003c
Wen Sihao <wensihao822@gmail.com>
2024-10-23 19:22:24 +0800

    Test telegram

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,5 +1,13 @@
 import "./style.css";
 import GameCard from "./GameCard";
+import { useEffect, useState } from "react";
+
+type TelegramUser = {
+  id: number;
+  first_name: string;
+  last_name?: string;
+  username?: string;
+};
 
 function Gallery() {
   const games = [
@@ -36,10 +44,31 @@ function Gallery() {
     // 更多游戏...
   ];
 
+  const [username, setUsername] = useState<string | null>(null);
+
+  useEffect(() => {
+    if ((window as any).Telegram?.WebApp) {
+      const tg = (window as any).Telegram.WebApp;
+      console.log(tg);
+      tg.expand();
+      const user: TelegramUser | undefined = tg.initDataUnsafe?.user;
+      if (user) {
+        setUsername(user.username || "NoName");
+      } else {
+        setUsername("Cannot get username");
+      }
+      tg.MainButton.setText("继续");
+      tg.MainButton.show();
+    } else {
+      setUsername("Telegram not found");
+    }
+  }, []);
+
   return (
     <div className="gallery">
       <header className="App-header">
         <h1 className="gallery-title">小游戏集合</h1>
+        <p>{username}</p>
       </header>
       <div className="game-list">
         {games.map((game, index) => (
